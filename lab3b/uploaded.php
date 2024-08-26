ed16e5ee432f57f260c71e2198a5bf94083d4a8f
ERLnotnice <earlgabrieldatu@gmail.com>
2024-08-26 22:30:32 +0800

    this is the image file upload

--- lab3b/uploaded.php
+++ lab3b/uploaded.php
@@ -3,19 +3,20 @@
 $upload_directory = getcwd() . '/uploads/';
 $relative_path = '/uploads/';
 
-// Handle Text File
-$uploaded_text_file = $upload_directory . basename($_FILES['text_file']['name']);
-$temporary_file = $_FILES['text_file']['tmp_name'];
+$uploaded_image_file = $upload_directory . basename($_FILES['image_file']['name']);
+$temporary_file = $_FILES['image_file']['tmp_name'];
 
-if (move_uploaded_file($temporary_file, $uploaded_text_file)) {
-    $text_file_content = file_get_contents($uploaded_text_file, 'r');
-    ?>
-    <textarea cols="70" rows="30"><?php echo $text_file_content; ?></textarea>
-    <?php
-} else {
-    echo 'Failed to upload file';
+if (!is_dir($upload_directory)) {
+    mkdir($upload_directory, 0755, true);
 }
 
+if (move_uploaded_file($temporary_file, $uploaded_image_file)) {
+    ?>
+    <img src="<?php echo $relative_path . basename($_FILES['image_file']['name']); ?>" alt="Uploaded Image" style="max-width: 100%; height: auto;">
+    <?php
+} else {
+    echo 'Failed to upload image';
+}
 
 echo '<pre>';
 var_dump($_FILES);
